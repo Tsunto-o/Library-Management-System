33b64249b45b8bad5df2d6157c13ecea229dd7fc
gv <gia.to@efrei.net>
2026-09-16 18:25:38 +0200

    16/09

--- src/app/main.java
+++ src/app/main.java
@@ -1,6 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class main{
+    public static void welcome() {
+        System.out.print("\nWelcome to the Library Management System !\n" +
+                "\n" +
+                "Choose an option :\n" +
+                " 1 - Login\n" +
+                " 2 - Register\n");
+    }
+
+    public static int readInputOption(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+
+        int res = -1;
+        while (res<min||res>max){
+            try {
+                System.out.printf("Enter an integer between %d and %d: ", min, max);
+                res = scanner.nextInt();
+                scanner.nextLine();
+            }catch (InputMismatchException e) {scanner.nextLine();}
+        }
+        return res;
+    }
+
     public static void main(String[] args){
-        System.out.println("test");
+
+        welcome();
+
+        int option = readInputOption(1, 2);
+        System.out.print(option);
+
+
     }
 
 }
---
+++ src/model/items/Magazine.java
@@ -0,0 +1,7 @@
+package model.items;
+
+import model.Borrowable;
+import model.LibraryItem;
+
+public class Magazine extends LibraryItem implements Borrowable {
+}
