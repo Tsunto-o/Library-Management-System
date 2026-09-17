1a4d81032506e8026e1204c3a88e3616e140e303
LucasRzM <piveron.lucas@outlook.fr>
2026-09-17 11:16:31 -0400

    User & loan done

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,35 @@
 package model;
 
-public abstract class User {
+public abstract class User implements Comparable<User> {
     private int stableId;
     private String name;
     private String rolePermissions;
+
+    public User(int stableId,String name, String rolePermissions) {
+        this.stableId=stableId;
+        this.name=name;
+        this.rolePermissions=rolePermissions;
+    }
+
+    public abstract String getDisplayInfo();
+
+    @Override
+    public String toString() {
+        return "Id: "+stableId+" Name: "+name+" Role permissions: "+rolePermissions;
+    }
+
+    public int getStableId() {
+        return stableId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getRolePermissions() {
+        return rolePermissions;
+    }
+
+    @Override
+    public int compareTo(User second) {
+        return Integer.compare(this.stableId,second.stableId);
+    }
 }
