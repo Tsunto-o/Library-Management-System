6fddf35aac665d738ac921ff2148c70ed57434ec
Zach <zachary.hue21@gmail.com>
2026-09-17 12:10:55 -0400

    Update Magazine.java

--- src/model/items/Magazine.java
+++ src/model/items/Magazine.java
@@ -3,4 +3,14 @@ package model.items;
 import model.Borrowable;
 
 public class Magazine extends LibraryItem implements Borrowable {
+    public Magazine(int stableId, String title, String publicationData, String status) {
+        super(stableId, title, publicationData, status);
+    }
+    @Override
+    public String getDisplayInfo() {
+        return "Magazine: " + getTitle()
+                + " (ID: " + getStableId()
+                + ", Publication date: " + getPublicationData()
+                + ", Status: " + getStatus() + ")";
+    }
 }
